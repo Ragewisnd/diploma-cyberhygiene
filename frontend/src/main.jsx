bf69dab36b868473d06eae6fc843153c51aed66c
Ragewisnd <61157880+Ragewisnd@users.noreply.github.com>
2026-03-24 04:51:52 +0300

    fix: import user.css in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./App.css";
 import "./admin.css";
+import "./user.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
